e8924f5f7b675dec36aded946b8328cf4db32bd9
Richard Mao <richmao@gmail.com>
2018-01-21 11:47:59 -0800

    soft final copy

--- node/public/javascripts/PinUser.js
+++ node/public/javascripts/PinUser.js
@@ -121,32 +121,8 @@ function saveData() {
     var address = escape(document.getElementById('address').value);
     var type = document.getElementById('type').value;
     var latlng = marker.getPosition();
-    var url = 'phpsqlinfo_addrow.php?name=' + name + '&address=' + address +
-        '&type=' + type + '&lat=' + latlng.lat() + '&lng=' + latlng.lng();
-
-    downloadUrl(url, function (data, responseCode) {
-
-        if (responseCode == 200 && data.length <= 1) {
-            infowindow.close();
-            messagewindow.open(map, marker);
-        }
-    });
-}
-
-function downloadUrl(url, callback) {
-    var request = window.ActiveXObject ?
-        new ActiveXObject('Microsoft.XMLHTTP') :
-        new XMLHttpRequest;
-
-    request.onreadystatechange = function () {
-        if (request.readyState == 4) {
-            request.onreadystatechange = doNothing;
-            callback(request.responseText, request.status);
-        }
-    };
-
-    request.open('GET', url, true);
-    request.send(null);
+    //post toilet
+    //post entry
 }
 
 // Drops Random Marker
